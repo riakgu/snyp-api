315050c767e593e07a652654a2ce562db21ee4ad
Rizky Akbar Gusnaen <rizkyakbargusnaen@gmail.com>
2025-11-12 12:28:25 +0700

    feat(link): add LinkStats model and migration

---
+++ prisma/migrations/20251112044913_create_table_link_stats/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "link_stats" (
+    "link_id" TEXT NOT NULL,
+    "total_visits" INTEGER NOT NULL DEFAULT 0,
+    "unique_visits" INTEGER NOT NULL DEFAULT 0,
+    "qr_visits" INTEGER NOT NULL DEFAULT 0,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "link_stats_pkey" PRIMARY KEY ("link_id")
+);
+
+-- AddForeignKey
+ALTER TABLE "link_stats" ADD CONSTRAINT "link_stats_link_id_fkey" FOREIGN KEY ("link_id") REFERENCES "links"("id") ON DELETE CASCADE ON UPDATE CASCADE;
