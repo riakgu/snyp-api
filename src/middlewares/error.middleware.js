f8998c4b30f930737cd2e7d37cc03be7ef2c7615
Rizky Akbar Gusnaen <rizkyakbargusnaen@gmail.com>
2025-12-29 11:27:09 +0700

    fix(security): hide error details in production

--- src/middlewares/error.middleware.js
+++ src/middlewares/error.middleware.js
@@ -1,5 +1,6 @@
 import { logger } from "../config/logger.js";
-import {ResponseError} from "../errors/response.error.js";
+import { ResponseError } from "../errors/response.error.js";
+import config from "../config/index.js";
 
 export function errorMiddleware(err, req, res, next) {
     if (err instanceof ResponseError) {
@@ -7,8 +8,14 @@ export function errorMiddleware(err, req, res, next) {
             errors: err.message
         });
     }
+
     logger.error(err);
+
+    const message = config.app.env === 'production'
+        ? 'Internal Server Error'
+        : err.message;
+
     res.status(500).json({
-        errors: err.message
+        errors: message
     });
 }
