a3fa3ff484cd5ce124beb4b7e83542ec9cfa16a9
Rizky Akbar Gusnaen <rizkyakbargusnaen@gmail.com>
2025-12-29 09:07:35 +0700

    refactor: consolidate workers folder into worker.js

--- src/worker.js
+++ src/worker.js
@@ -1,6 +1,32 @@
-import { connectRabbitMQ } from './config/rabbitmq.js';
-import { startStatsWorker } from "./workers/stats.worker.js";
-import {shutdown} from "./config/shutdown.js";
+import { connectRabbitMQ, getChannel, STATS_QUEUE } from './config/rabbitmq.js';
+import { shutdown } from "./config/shutdown.js";
+import { logger } from "./config/logger.js";
+import statsService from './services/stats.service.js';
+
+async function startStatsWorker() {
+    const channel = getChannel();
+
+    await channel.prefetch(1);
+
+    logger.info('Stats worker started, waiting for messages...');
+
+    channel.consume(
+        STATS_QUEUE,
+        async (msg) => {
+            if (!msg) return;
+
+            try {
+                const data = JSON.parse(msg.content.toString());
+                await statsService.processVisitEvent(data);
+                channel.ack(msg);
+            } catch (err) {
+                logger.error('Error processing visit:', err);
+                channel.nack(msg, false, true);
+            }
+        },
+        { noAck: false }
+    );
+}
 
 await connectRabbitMQ();
 await startStatsWorker();
